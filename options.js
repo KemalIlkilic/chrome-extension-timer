bdd957810f51e1a72767bba789478efae7ab97a0
KemalIlkilic <kemalonur.ilkilic@bahcesehir.edu.tr>
2024-01-03 23:58:36 +0300

    Add storage permission, chrome.storage and name input functionality

--- options.js
+++ options.js
@@ -1,7 +1,18 @@
 const nameInput = document.getElementById("name-input");
 const saveBtn = document.getElementById("save-btn");
 
+// callback functionlara dikkat et () => {} gibi.
+// Event Handling, Promises and async functions, Array methods map() gibi, buralarda kullaniliyor.
 saveBtn.addEventListener("click", () => {
   const name = nameInput.value;
-  console.log(name);
+  chrome.storage.sync.set({ name: name }, () => {
+    console.log(`Saved name: ${name}`);
+  });
+});
+
+// get ile ['a', 'b'] diyerek birden fazla key icin birden fazla value alabilirsin.
+// sonuc olarak data objesi aliyorsun. data = {name: 'babam'}
+chrome.storage.sync.get(["name"], (data) => {
+  nameInput.value = data.name ?? "???";
+  console.log(data);
 });
--- popup.js
+++ popup.js
@@ -2,6 +2,13 @@ const timeElement = document.getElementById("time");
 const currentTime = new Date().toLocaleTimeString();
 timeElement.textContent = `The current time is ${currentTime}`;
 
+// konu ile alakasi yok 4 karakterlik label icin kullaniliyor.
 chrome.action.setBadgeText({ text: "TIME" }, () =>
   console.log("Badge text set")
 );
+
+const nameElement = document.getElementById("name");
+chrome.storage.sync.get(["name"], (data) => {
+  const name = data.name ?? "???";
+  nameElement.textContent = `Hello ${name}`;
+});
